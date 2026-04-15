dd95d358ede45ae08975ec5f9aecd8f48a147ef2
Kunalpipaliya <kunalpipaliya67@gmail.com>
2026-04-15 11:56:55 +0530

    database connection and create operation

--- server.js
+++ server.js
@@ -1,7 +1,54 @@
-const express=require('express')
-const app=express()
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
 
-app.get("/",(req,res)=>{
-    res.render('app.ejs')
-})
-app.listen("2026")
+const sql = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "kp",
+});
+
+sql.connect((err) => {
+  if (err) throw err;
+  console.log("connection success");
+});
+app.get("/", (req, res) => {
+  const qry = "select * from cineflow";
+
+  sql.query(qry, (err, data) => {
+    if (err) throw err;
+
+    res.render("app.ejs", { data });
+  });
+});
+
+app.get("/admin", (req, res) => {
+  res.render("admin.ejs");
+});
+
+app.get("/createData", (req, res) => {
+  const { title, image, year, director, genre, description } = req.query;
+
+  const qry = `INSERT INTO cineflow (title,image,year,director,genre,description) VALUES ('${title}','${image}',${year},'${director}','${genre}','${description}')`;
+
+  sql.query(qry, (err) => {
+    if (err) throw err;
+    console.log("data enter success");
+  });
+
+  res.redirect("/");
+});
+
+app.get("/detail/:cid", (req, res) => {
+  const id = req.params.cid;
+
+  const qry = `select * from cineflow where cid=${id}`;
+  sql.query(qry,(err,data)=>{
+    if(err) throw err
+    res.render('movie.ejs',{data})
+  })
+
+});
+
+app.listen("2026");
